b80116d78abc4fdbecb6bb54110bcf5256c33b3f
Félix Mariotto <felix.mariotto@gmail.com>
2025-04-06 23:40:14 +0200

    create unfinished basic 2D physics example

---
+++ 2d-physics/player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+@export var speed := 300.0
+var direction := Vector2()
+
+func _physics_process(delta: float) -> void:
+	
+	# reset direction
+	direction.x = 0.0
+	direction.y = 0.0
+	if Input.is_action_pressed("ui_down"):
+		direction.y += 1.0
+	if Input.is_action_pressed("ui_up"):
+		direction.y -= 1.0
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1.0
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1.0
+	direction = direction.normalized()
+	
+	var collision_info = move_and_collide( direction * delta * speed )
+	if collision_info:
+		print( collision_info )
