60b1de835c3836da873eb26cc1d7fca29517b9ee
Félix Mariotto <felix.mariotto@gmail.com>
2025-04-09 21:54:54 +0200

    2D physics: apply impulse to the body colliding the player

--- 2d-physics/game.gd
+++ 2d-physics/game.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
+@export var spawn_interval := 1.0
+
 var Ball = preload("res://ball.tscn")
 var timer = 0.0
 
 func _physics_process(delta: float) -> void:
 	timer += delta
 	# spawn a ball
-	if timer >= 1.0:
+	if timer >= spawn_interval:
 		timer = 0.0
 		var spawn_position = $BallsPath.curve.sample( 0, randf() )
 		if Ball:
--- 2d-physics/player.gd
+++ 2d-physics/player.gd
@@ -1,27 +1,29 @@
 extends CharacterBody2D
 
 @export var speed := 300.0
-var direction := Vector2()
+var input_direction := Vector2()
 
 func _physics_process(delta: float) -> void:
 	
 	# reset direction
-	direction.x = 0.0
-	direction.y = 0.0
+	input_direction.x = 0.0
+	input_direction.y = 0.0
 	if Input.is_action_pressed("ui_down"):
-		direction.y += 1.0
+		input_direction.y += 1.0
 	if Input.is_action_pressed("ui_up"):
-		direction.y -= 1.0
+		input_direction.y -= 1.0
 	if Input.is_action_pressed("ui_left"):
-		direction.x -= 1.0
+		input_direction.x -= 1.0
 	if Input.is_action_pressed("ui_right"):
-		direction.x += 1.0
-	direction = direction.normalized()
+		input_direction.x += 1.0
+	input_direction = input_direction.normalized()
 	
-	var collision_info = move_and_collide( direction * delta * speed )
+	var collision_info = move_and_collide( input_direction * delta * speed )
 	
 	if collision_info:
-		move_and_collide( collision_info.get_remainder().slide( collision_info.get_normal() ) )
+		var collision_normal = collision_info.get_normal()
+		move_and_collide( collision_info.get_remainder().slide( collision_normal ) )
 		var collider = collision_info.get_collider()
 		# here we should apply an impulse on the collider object
-		print( collider )
+		if collider is RigidBody2D:
+			collider.apply_impulse( collision_info.get_collider_velocity() * -0.1, collision_info.get_position() - collider.position )
